d5d9762338af2baa7e218fed6c1d552a0f61cd83
DataUniverse <pjh@datau.co.kr>
2020-12-14 16:55:52 +0900

    first commit

---
+++ defensecall/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  defensecall
+//
+//  Created by DataUniverse on 2020/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ defensecall/defensecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  defensecallApp.swift
+//  defensecall
+//
+//  Created by DataUniverse on 2020/12/14.
+//
+
+import SwiftUI
+
+@main
+struct defensecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
